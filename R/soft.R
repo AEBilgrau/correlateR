4df08db9fa2ee7305fe22b183c30e9ce62cc424b
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-09-02 11:54:50 +0200

    Added the soft thresholding function.

---
+++ R/soft.R
@@ -0,0 +1,18 @@
+#' The soft thresholding function
+#' 
+#' The soft thresholding function given by
+#' \deqn{\text{soft}(a, c) = \text{sgn}(a)\cdot(|a| - c)_+}{
+#'       soft(a, c) = sgn(a)*max(|a| - c, 0)}
+#' @param a A vector/matrix to apply the soft thresholding to.
+#' @param c A numeric giving the soft threshold.
+#' @return A vector/matrix the same size as \code{a}.
+#' @examples
+#' a <- rnorm(100)
+#' soft(a, c = 0.2)
+#'   
+#' x <- createData(n = 10, m = 4)
+#' soft(x, c = 0.7)
+#' @export
+soft <- function(a, c) {
+  return(sign(a)*pmax(abs(a) - c, 0))
+}
