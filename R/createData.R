ec24367c926f5bad38129577acf05931693cd554
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-09-02 10:57:56 +0200

    Fixed degenerate and n by 1 and 1 by m cases.

--- R/createData.R
+++ R/createData.R
@@ -1,20 +1,29 @@
 #' Create toy data
 #' 
 #' Creates a named matrix of toy data from an \code{m}-dimensional 
-#' zero-mean normal distribution with identity covariance matrix.
+#' zero-mean normal distribution with identity covariance matrix for 
+#' convenience.
 #' 
-#' @param n an integer giving the number of observations/samples.
-#' @param m an integer giving the number of variables/features.
-#' @param n.na an integer giving the number of \code{NA}s in the output.
-#'   Default is \code{0}.
-#' @return A \code{n} times \code{m} numeric matrix of observations.
-#' @note The \code{NA} are randomly inserted in the output.
+#' @param n an non-negative integer giving the number of observations/samples.
+#' @param m an non-negative integer giving the number of variables/features.
+#' @param n.na an integer from \code{0} to \code{n*m} giving the number of 
+#'   \code{NA}s in the output. Default is \code{0}.
+#' @return A \code{n} times \code{m} numeric matrix of observations from
+#'   zero-mean gaussian random variables.
+#' @note The \code{n.na} \code{NA}s are randomly inserted in the output.
 #' @examples
 #' createData(10, 4)
+#' createData(1, 2)
+#' 
+#' # Also works for the degenerate cases!
+#' createData(3, 0)
+#' createData(0, 3)
 #' @export
 createData <- function(n, m, n.na = 0) {
-  X <- replicate(m, rnorm(n))
-  dimnames(X) <- list(paste0("obs", 1:n), paste0("var", 1:m))
+  stopifnot(n >= 0 && m >= 0)
+  X <- matrix(rnorm(n*m), n, m,
+              dimnames = list(sprintf("obs%03d", seq_len(n)),
+                              sprintf("var%03d", seq_len(m))))
   if (n.na) {
     stopifnot(n.na >= 0 && n.na <= n*m)
     X[sample(n*m, n)] <- NA
