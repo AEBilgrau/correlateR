d67b064be1a9c17ab7064109c0319c7dbc57375c
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-10-24 13:37:54 +0200

    Fixed issues in fit.grem.moment. Changed order of arguments in fit.grem.MLE and fit.grem.moment. They are also no longer exported.  Minor clean-up.

--- R/GREM.R
+++ R/GREM.R
@@ -36,7 +36,7 @@ fit.grem <- function(S,
                      eps = 1e-3,
                      verbose = FALSE) {
   p <- nrow(S)
-  interval <- c(p - 1 + 1e-5, 1e6)
+  interval <- c(p - 1 + 10*.Machine$double.eps, 1e6)
   Psi.old <- Psi.init
   nu.old  <- nu.init
   for (i in seq_len(max.ite)) {
@@ -62,24 +62,19 @@ fit.grem <- function(S,
 
 
 # MLE alg
-#' @export
-fit.grem.MLE <- function(nu.init, S, ns,
+fit.grem.MLE <- function(S, ns,
+                         nu.init = nrow(S[[1]]) + 2,
                          max.ite = 1000, eps = 1e-3,
                          verbose = FALSE) {
   p <- nrow(S)
-  interval <- c(p - 1 + 1e-5, 1e6)
-  
+  interval <- c(p - 1 + 10*.Machine$double.eps, 1e6)
   nu.old <- nu.init
   Psi.old <- grem_mle_step(nu = nu.old, S = S, ns = ns)
-  
   for (i in seq_len(max.ite)) {
     ll.old <- grem_loglik_arma(Psi.old, nu.old, S, ns)
-    
     nu.new  <- grem_get_nu(Psi.old, nu.old, S, ns, interval)
     Psi.new <- grem_mle_step(nu.new, S, ns)
-    
     ll.new  <- grem_loglik_arma(Psi.new, nu.new, S, ns)
-    
     stopifnot(ll.new > ll.old)
     if (ll.new - ll.old < eps) {
       break
@@ -87,12 +82,10 @@ fit.grem.MLE <- function(nu.init, S, ns,
       Psi.old <- Psi.new
       nu.old  <- nu.new
     }
-    
     if (verbose) {
       cat("ite =", i, ":", "ll.new - ll.old =", ll.new - ll.old, "\n");
       flush.console()
-    }
-    
+    } 
   }
   
   if (i == max.ite) warning("max iterations (", max.ite, ") hit!")
@@ -101,34 +94,31 @@ fit.grem.MLE <- function(nu.init, S, ns,
 }
 
 # Estimation using moment
-#' @export
-fit.grem.moment <- function(nu.init, S, ns,
+fit.grem.moment <- function(S, ns,
+                            nu.init = nrow(S[[1]]) + 10,
                             max.ite = 1000, eps = 1e-3,
                             verbose = FALSE) {
   p <- nrow(S[[1]])
-  interval <- c(p - 1 + 1e-5, 1e6)
-  
-  nu.old <- nu.init
-  Psi.old <- grem_moment_step(nu = nu.old, S = S, ns = ns)
-  
+  interval <- c(p - 1 + 10*.Machine$double.eps, 1e6)
+  nu.old   <- nu.init
+  Psi.old  <- grem_moment_step(nu = nu.old, S = S, ns = ns)
   for (i in seq_len(max.ite)) {
-    nu.new  <- grem_get_nu(Psi.old, nu.old, S, ns, interval)
-    fac     <- (nu.new - p - 1)/(nu.old - p - 1)
-    Psi.new <- Psi.old * fac
-    
+    nu.new  <- grem_get_nu(Psi = Psi.old, nu = nu.old, S = S, ns = ns,
+                           interval = interval)
+#     fac <- (nu.new - p - 1)/(nu.old - p - 1)
+#     Psi.new <- Psi.old * fac
+    Psi.new  <- grem_moment_step(nu = nu.old, S = S, ns = ns)
     if (verbose) {
       cat("ite =", i, ":", "nu.new - nu.old =", nu.new - nu.old,
           "fac =", fac, "nu =", nu.new, "\n");
       flush.console()
     }
-    
     if (abs(nu.new - nu.old) < eps) {
       break
     } else {
       Psi.old <- Psi.new
       nu.old  <- nu.new
     }
-    
   }
   if (i == max.ite) warning("max iterations (", max.ite, ") hit!")
   return(list("Psi" = Psi.new, "nu" = nu.new, "iterations" = i))
