2cff57121b11a8dc0a0969ce7bb2da7f5fb57d30
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-10-27 14:12:27 +0100

    Compute scatter and cross-scatter matrix.

--- R/scatter.R
+++ R/scatter.R
@@ -5,10 +5,11 @@
 #'   \deqn{\sum_{i = 1}^n(x_i-\mu)(x_i-\mu)^T}{XX^T} and \eqn{XY^T}{XY^T} 
 #' respectively.
 #' 
-#' @param X An \code{n} by \code{p1} numeric matrix with observations in the rows
-#'   and variables in the columns.
+#' @param X An \code{n} by \code{p1} numeric matrix with observations in the 
+#'   rows and variables in the columns.
 #' @param Y An \code{n} by \code{p2} as \code{X}.
-#' @return The scatter matrix or cross-scatter matrix.
+#' @return The \code{p1} times \code{p1} scatter matrix or 
+#'   \code{p1} times \code{p2} cross-scatter matrix.
 #' @author Anders Ellern Bilgrau <abilgrau (at) math.aau.dk>
 #' @examples
 #' n <- 11
@@ -19,13 +20,13 @@
 #' 
 #' correlateR:::xscatter(X, Y)
 #' n*xcov(X, Y, method = "ML")  # == (n - 1)*xcov(X, Y)
-#' @keywords internal
+#' @export
 scatter <- function(X) {
   return(crossprod(center(X)))
 }
 
 #' @rdname scatter
-#' @keywords internal
+#' @export
 xscatter <- function(X, Y) {
   return(crossprod(center(X), center(Y)))
 }
