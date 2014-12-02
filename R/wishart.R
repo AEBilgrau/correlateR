b8b7c15771b885972bfdde3379379abe3a7c9fbb
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-12-02 19:47:33 +0100

    rwishart and rinvwishart now uses the RcppAmadillo versions. Updated docs.

--- R/wishart.R
+++ R/wishart.R
@@ -5,7 +5,7 @@
 #'
 #' @param n The number of realizations.
 #' @param sigma A \code{p} by \code{p} symmetric postive-definite matrix.
-#' @param nu An integer giving the degrees of freedom.
+#' @param nu An numeric of length one giving the degrees of freedom.
 #' @return \code{rwishart} returns an \code{p} by \code{p} by \code{n} 
 #'   3-dimensional array where each slice is a draw from the Wishart 
 #'   distribution.
@@ -13,39 +13,23 @@
 #'   \code{\link{rWishart}} from the \pkg{stats}-package.
 #' @examples
 #' rwishart(n = 2)
-#' rWishart(n = 2, df = 10L, Sigma = diag(3))
 #' rwishart(n = 3, diag(4), nu = 4)
-#' n <- 1000
-#' a <- replicate(n,diag(3)) - rwishart(n)/10L
-#' hist(a)
 #' @export
-rwishart <- function(n, sigma = diag(3), nu = 10L) {
+rwishart <- function(n, sigma = diag(3), nu = 10) {
   stopifnot(nrow(sigma) == ncol(sigma))
-  p <- ncol(sigma)
-  if (nu <= p - 1) {
-    warning("For the matrix to be invertible n > p - 1 must hold")
-  }
-  L <- chol(sigma)
-  A <- matrix(0, p, p)
-  ans <- replicate(n, {
-    A[lower.tri(A)] <- rnorm(p*(p-1)/2)
-    diag(A) <- sqrt(rchisq(p, nu - seq_len(p) + 1))
-    crossprod(L %*% A)
-  })
-  return(ans)
+  stopifnot(nu > ncol(sigma) - 1)
+  return(rwishartArma(n, sigma, nu))
 }
 
-
 #' @rdname rwishart
-#' @param Psi A square symmetric postive-definite matrix.
+#' @param psi A square symmetric postive-definite matrix.
 #' @return For \code{rinvwishart} each slice is a draw from the inverse Wishart 
 #'   distribution.
 #' @examples
 #' rinvwishart(n = 2)
 #' @export
-rinvwishart <- function(n, Psi = diag(3), nu = 10L) {
-  stopifnot(nrow(Psi) == ncol(Psi))
-  stopifnot(nu > ncol(Psi) - 1)
-  ans <- replicate(n, solve(rwishart(n = 1, sigma = solve(Psi), nu = nu)[,,1]))
-  return(ans)
+rinvwishart <- function(n, psi = diag(3), nu = 10) {
+  stopifnot(nrow(psi) == ncol(psi))
+  stopifnot(nu > ncol(psi) - 1)
+  return(rinvwishartArma(n, psi, nu))
 }
