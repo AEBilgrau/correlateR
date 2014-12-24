f034283d1caed8bf40dc98da6cd3187d36ef09e4
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-12-24 15:52:53 +0100

    Added possibility of simulating from a singular wishart distribution.

--- R/wishart.R
+++ R/wishart.R
@@ -2,10 +2,15 @@
 #'
 #' Generate data from Wishart and inverse-Wishart distributions using the 
 #' Barlett decomposition.
+#' When simulating from the Wishart distribution and \code{nu} is an integer
+#' strictly less than p, then the functions draws from a singular Wishart 
+#' distribution using multivariate normal draws.
 #'
 #' @param n The number of realizations.
 #' @param sigma A \code{p} by \code{p} symmetric postive-definite matrix.
 #' @param nu An numeric of length one giving the degrees of freedom.
+#' @param verbose logical. Warn if draws are from a singular Wishart 
+#'   distribution.
 #' @return \code{rwishart} returns an \code{p} by \code{p} by \code{n} 
 #'   3-dimensional array where each slice is a draw from the Wishart 
 #'   distribution.
@@ -14,10 +19,24 @@
 #' @examples
 #' rwishart(n = 2)
 #' rwishart(n = 3, diag(4), nu = 4)
+#' 
+#' rwishart(n = 1, diag(3), nu = 3)
+#' rwishart(n = 1, diag(3), nu = 2)
+#' rwishart(n = 1, diag(3), nu = 1)
+#' @importFrom GMCM rmvnormal
 #' @export
-rwishart <- function(n, sigma = diag(3), nu = 10) {
+rwishart <- function(n, sigma = diag(3), nu = 10, verbose = TRUE) {
   stopifnot(nrow(sigma) == ncol(sigma))
-  stopifnot(nu > ncol(sigma) - 1)
+  if (nu <= ncol(sigma) - 1) {
+    if (!all.equal(nu, as.integer(nu))) {
+      stop("When nu <= p - 1 then nu must be an integer.")
+    }
+    if (verbose) {
+      warning("Simulating from a singular Wishart distribution as nu <= p - 1.")
+    }
+    mu <- rep(0, nrow(sigma))
+    return(replicate(n, scatter(rmvnormal(nu, mu = mu, sigma = sigma))))
+  }
   return(rwishartArma(n, sigma, nu))
 }
 
