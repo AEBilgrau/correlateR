a2b785d7a6a8d7ffc929d40038ec54c80b4f374b
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2019-05-08 21:12:48 +0200

    Fixed degenerate cases for (x)covArma.

--- src/covFamily.cpp
+++ src/covFamily.cpp
@@ -71,8 +71,8 @@ Rcpp::NumericMatrix covRcpp(Rcpp::NumericMatrix & X,
 //' @export
 // [[Rcpp::export]]
 Rcpp::NumericMatrix xcovRcpp(Rcpp::NumericMatrix & X,
-                                 Rcpp::NumericMatrix & Y,
-                                 const int norm_type = 0) {
+                             Rcpp::NumericMatrix & Y,
+                             const int norm_type = 0) {
   
   const int n = X.nrow();
   const int m_X = X.ncol();
@@ -99,9 +99,20 @@ Rcpp::NumericMatrix xcovRcpp(Rcpp::NumericMatrix & X,
 //' @rdname covFamily
 //' @export
 // [[Rcpp::export]]
-arma::mat covArma(const arma::mat & X,
+arma::mat covArma(const arma::mat& X,
                   const int norm_type = 0) {
-  return arma::cov(X, norm_type);
+  arma::mat out(X.n_cols, X.n_cols);
+  
+  // Degenerate cases 
+  if (X.n_cols == 0) {
+    return out;
+  } else if (X.n_rows == 0 || X.n_rows == 1) {
+    out.fill(Rcpp::NumericVector::get_na());
+  } else {
+    out = arma::cov(X, norm_type);
+  }
+  
+  return out;
 }
 
 
@@ -109,12 +120,22 @@ arma::mat covArma(const arma::mat & X,
 //' @rdname covFamily
 //' @export
 // [[Rcpp::export]]
-arma::mat xcovArma(const arma::mat & X,
-                   const arma::mat & Y,
+arma::mat xcovArma(const arma::mat& X,
+                   const arma::mat& Y,
                    const int norm_type = 0) {
-  return arma::cov(X, Y, norm_type);
+  arma::mat out(X.n_cols, Y.n_cols);
+  
+  // Degenerate case first
+  if (X.n_cols == 0 || Y.n_cols == 0) {
+    return out;
+  } else if (X.n_rows == 0 || X.n_rows == 1 || Y.n_rows == 0 || Y.n_rows == 1) {
+    out.fill(Rcpp::NumericVector::get_na());
+  } else {
+    out = arma::cov(X, Y, norm_type);
+  }
+  
+  return out;
 }
-
 
 // covariance in Eigen
 //' @rdname covFamily
@@ -154,27 +175,4 @@ Eigen::MatrixXd xcovEigen(Eigen::Map<Eigen::MatrixXd> & X,
     // Return the X^T * X is the scatter matrix
     return X.transpose() * Y / df;
 }
-
-
-
-
-/*** R
-library("microbenchmark")
-
-X <- replicate(10, rnorm(50))
-dimnames(X) <- list(paste0("obs", 1:nrow(X)), paste0("dim", 1:ncol(X)))
-Y <- replicate(15, rnorm(50))
-dimnames(Y) <- list(paste0("obs", 1:nrow(Y)), paste0("dim", 1:ncol(Y)))
-
-covArma(X, 0)
-covRcpp(X, 0)
-covEigen(X, 0)
-all.equal(covArma(X, 0), covRcpp(X, 0))
-all.equal(covArma(X, 1), covRcpp(X, 1))
-
-all(crosscovRcpp(X, Y, 1) == crosscovArma(X, Y, 1))
-print(microbenchmark(crosscovArma(X, Y, 0), 
-                     crosscovEigen(X, Y, 0),
-                     crosscovRcpp(X, Y, 0), times = 10), order = "median")
-*/
 
