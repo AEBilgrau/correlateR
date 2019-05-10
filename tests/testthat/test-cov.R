97b227e85714258e0e7896e92d841295bc8760e7
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2019-05-10 19:31:20 +0200

    Update tests.

--- tests/testthat/test-cov.R
+++ tests/testthat/test-cov.R
@@ -3,7 +3,7 @@ context("Marginal covariance tests")
 # Testing data
 n <- 50
 m_X <- 10
-m_Y <-15
+m_Y <- 15
 X <- createData(n, m_X)
 Y <- createData(n, m_Y)
 
@@ -24,13 +24,15 @@ test_that("Covariance is computed correctly", {
   expect_that(cov_X*(n - 1)/n, equals(cov(X, method = "ML")))
 })
 
+
+
 test_that("Cross-correlation is computed correctly", {
-  expect_that(cov_XY,           is_equivalent_to(xcovArma(X, Y, 0)))
-  expect_that(cov_XY*(n - 1)/n, is_equivalent_to(xcovArma(X, Y, 1)))
-  expect_that(cov_XY,           is_equivalent_to(xcovEigen(X, Y, 0)))
-  expect_that(cov_XY*(n - 1)/n, is_equivalent_to(xcovEigen(X, Y, 1)))
-  expect_that(cov_XY,           equals(xcov(X, Y)))
-  expect_that(cov_XY*(n - 1)/n, equals(xcov(X, Y, method = "ML")))
+  expect_equivalent(cov_XY,           xcovArma(X, Y, 0))
+  expect_equivalent(cov_XY*(n - 1)/n, xcovArma(X, Y, 1))
+  expect_equivalent(cov_XY,           xcovEigen(X, Y, 0))
+  expect_equivalent(cov_XY*(n - 1)/n, xcovEigen(X, Y, 1))
+  expect_equal(cov_XY,           xcov(X, Y))
+  expect_equal(cov_XY*(n - 1)/n, xcov(X, Y, method = "ML"))
 })
 
 # Test for degenerate cases
@@ -40,20 +42,20 @@ for (i in seq_len(nrow(combs))) {
   n <- combs$n[i]
   m_X <- combs$m_X[i]
   m_Y <- combs$m_Y[i]
-  x <- createData(n, m_X)
-  y <- createData(n, m_Y)
-  cov_x <- stats::cov(x)
-  cov_xy <- stats::cov(x, y)
-  
+  X <- createData(n, m_X)
+  Y <- createData(n, m_Y)
+  cov_X <- stats::cov(X)
+  cov_XY <- stats::cov(X, Y)
+
   test_that(sprintf("Covariance function works in degenerate cases, n = %d, m = %d", n, m_X), {
-    expect_equal(cov_x, covArma(x))
-    expect_equal(cov_x, covEigen(x))
-    expect_equal(cov_x, covRcpp(x)) 
+    expect_equivalent(cov_X, covArma(X))
+    expect_equivalent(cov_X, covEigen(X))
+    expect_equivalent(cov_X, covRcpp(X))
   })
-  
+
   test_that(sprintf("Cross-covariance function works in degenerate cases, n = %d, m_X = %d, m_Y = %d", n, m_X, m_Y), {
-    expect_equal(cov_xy, xcovArma(x, y))
-    expect_equal(cov_xy, xcovEigen(x, y))
-    expect_equal(cov_xy, xcovRcpp(x, y)) 
+    expect_equivalent(cov_XY, xcovArma(X, Y))
+    expect_equivalent(cov_XY, xcovEigen(X, Y))
+    expect_equivalent(cov_XY, xcovRcpp(X, Y))
   })
 }
